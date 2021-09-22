7683850792c00e017050dc5de51c2d896294eb07
AlexanderYa98 <ayakovlev524@gmail.com>
2021-09-22 08:54:05 +0700

    2.1.2

---
+++ src/main/java/App.java
@@ -0,0 +1,28 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2.getMessage());
+
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+
+        boolean comparison =  bean == bean1;
+        boolean comparison1 =  bean2 == bean3;
+        System.out.println(comparison);
+        System.out.println(comparison1);
+
+    }
+}
